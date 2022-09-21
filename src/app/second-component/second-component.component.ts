8cbaa366a1f9515d9f288b6f7d785147a43dd9fe
renanrcs <renan.cavalcante@yahoo.com>
2022-09-21 19:03:23 -0300

    creating second component with ng and using pipes

---
+++ src/app/second-component/second-component.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-second-component',
+  templateUrl: './second-component.component.html',
+  styleUrls: ['./second-component.component.css']
+})
+export class SecondComponentComponent {
+
+  name = "Renan";
+
+}
