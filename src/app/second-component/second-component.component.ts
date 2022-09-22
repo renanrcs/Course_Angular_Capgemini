a873e0f4d1ae360e45753350a0cb4cdd9cb57daa
renanrcs <renan.cavalcante@yahoo.com>
2022-09-22 20:49:49 -0300

    learning about property_binding and event_binding

--- src/app/second-component/second-component.component.ts
+++ src/app/second-component/second-component.component.ts
@@ -6,8 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./second-component.component.css']
 })
 export class SecondComponentComponent {
-
   name = "Renan";
   dataNascimento = "1991/10/16";
+  urlImage = "/assets/logo_proway.png"
 
+  mostrarDataNascimento(){
+    alert(`A data de nascimento do ${this.name} é: ${this.dataNascimento}`);
+  }
 }
