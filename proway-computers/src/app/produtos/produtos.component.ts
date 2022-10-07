777b14b229e08b684a8477cb832e887499fd9aa8
renanrcs <renan.cavalcante@yahoo.com>
2022-10-07 15:00:34 -0300

    creating page of products

---
+++ proway-computers/src/app/produtos/produtos.component.ts
@@ -0,0 +1,18 @@
+import { produtos, IProduto } from './../produtos';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-produtos',
+  templateUrl: './produtos.component.html',
+  styleUrls: ['./produtos.component.css']
+})
+export class ProdutosComponent implements OnInit {
+
+  produtos: IProduto[] = produtos;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
