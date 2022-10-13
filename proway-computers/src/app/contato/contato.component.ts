220981ecfc36b7ee09fa0f29057c00c591697c4c
renanrcs <renan.cavalcante@yahoo.com>
2022-10-13 14:50:55 -0300

    implementing reactive form

--- proway-computers/src/app/contato/contato.component.ts
+++ proway-computers/src/app/contato/contato.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contato',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContatoComponent implements OnInit {
 
-  constructor() { }
+  formContact = this.formBuilder.group({
+    nome: ["", [
+      Validators.minLength(4),
+      Validators.required
+    ]],
+    assunto: ["", [
+      Validators.minLength(10),
+      Validators.required
+    ]],
+    telefone: ["", [
+      Validators.minLength(11),
+      Validators.required
+    ]],
+    email: ["", [
+      Validators.email,
+      Validators.required
+    ]],
+    mensagem: ["", [
+      Validators.minLength(20),
+      Validators.required
+    ]]
+  });
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  sendForm(){
+    alert(`Formulario enviado com sucesso!`);
+    this.formContact.reset();
   }
 
 }
